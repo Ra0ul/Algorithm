c80714424f9b5568df5eb8174882c77c4cc228bf
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-05-04 22:30:23 +0900

    [unrated] Title: ad 제거하기, Time: 0.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.py
+++ 프로그래머스/unrated/181870. ad 제거하기/ad 제거하기.py
@@ -1,11 +1,7 @@
 def solution(strArr):
-    answer = []
-    """
-    배열중에서 'ad'  글자가 붙어있는 원소를 찾으면 무조건 제외해주기
-    """
-    
-    for i in strArr:
-        if i.find('ad') == -1:
-            answer.append(i)
-        
-    return answer
+
+    for i in strArr[::]:
+        if 'ad' in i:
+            strArr.remove(i)
+            
+    return strArr
