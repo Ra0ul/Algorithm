e5e3e321ac057f800377d29948638f050b5f0b8c
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-07-16 00:15:00 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -3,14 +3,12 @@ def solution(array):
     list = []
 
     for i in array:
-# list에 i번째에 value 1추가하기 
-# 3의 경우에는 3번 count되니까 value가 3
         try:
             doc[i] += 1
         except:
             doc[i] = 1
 
-        # doc = {"1" : 1, "2" : 1, "3" : 3}
+     # doc = {"1" : 1, "2" : 1, "3" : 3}
 
     count = max(doc.values())  # 가장 큰 value 체크하기 : 2
     index = doc.items()  # 딕셔너리에 키값과 value값 동시에 불러오기
