da45aaf540d777143082fbaedd7021c49a90432e
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-07-21 23:48:03 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,14 +1,7 @@
 def solution(num, total):
     answer = [] 
     num_list = 0
-    """
-    연속한수 Num개
-    더한값 total
-    이되는 배열을 오름차순으로 담아 보내기!
-    x, x+1,... x+(num-1)
     
-    total = num*x + (0+1+2+3 ... + num-1)
-    """
     for i in range(0, num):
         num_list += i 
     x = int((total - num_list)/num)
