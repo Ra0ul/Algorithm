317c2d6cee6d138b8df723292143b54b6148f4aa
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-07-21 21:46:59 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -0,0 +1,16 @@
+def solution(dots):
+    answer = 0
+    x_group=[]
+    y_group=[]
+    """
+    직사각형의 넓이 =  가로*세로
+    가로 = x 좌표의 차이 (dot에서 두값)
+    세로 = y 좌표의 차이 (dot에서 두값)
+    """
+    for i in range(0,3):
+        if dots[i][0] not in x_group:
+            x_group.append(dots[i][0])
+        if dots[i][1] not in y_group:
+            y_group.append(dots[i][1])
+    answer = abs((x_group[0]-x_group[1])*(y_group[0]-y_group[1]))
+    return answer
