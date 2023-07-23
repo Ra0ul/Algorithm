e44be3ae26c5f393b2c7b0e771663c293faa5035
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-07-23 18:48:53 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,10 +1,5 @@
 def solution(my_string, num1, num2):
-    answer = ''
     new_list = list(my_string)
-    new_list[num1] = my_string[num2]
-    new_list[num2] = my_string[num1]
-    
-    for i in new_list:
-        answer += i
+    new_list[num1], new_list[num2] = new_list[num2], new_list[num1]
         
-    return answer
+    return ''.join(new_list)
