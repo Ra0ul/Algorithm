21139339b2c4fab63fd0486dca2865fc3b90f498
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-07-23 19:26:46 +0900

    [level 0] Title: 직사각형 넓이 구하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
+++ 프로그래머스/lv0/120860. 직사각형 넓이 구하기/직사각형 넓이 구하기.py
@@ -1,20 +1,16 @@
 def solution(dots):
-    
-    return(max(dots)[0]-min(dots)[0]) * (max(dots)[1]-min(dots)[1])
-    
-    
-    # answer = 0
-    # x_group=[]
-    # y_group=[]
-    # """
-    # 직사각형의 넓이 =  가로*세로
-    # 가로 = x 좌표의 차이 (dot에서 두값)
-    # 세로 = y 좌표의 차이 (dot에서 두값)
-    # """
-    # for i in range(0,3):
-    #     if dots[i][0] not in x_group:
-    #         x_group.append(dots[i][0])
-    #     if dots[i][1] not in y_group:
-    #         y_group.append(dots[i][1])
-    # answer = abs((x_group[0]-x_group[1])*(y_group[0]-y_group[1]))
-    return 0
+    answer = 0
+    x_group=[]
+    y_group=[]
+    """
+    직사각형의 넓이 =  가로*세로
+    가로 = x 좌표의 차이 (dot에서 두값)
+    세로 = y 좌표의 차이 (dot에서 두값)
+    """
+    for i in range(0,3):
+        if dots[i][0] not in x_group:
+            x_group.append(dots[i][0])
+        if dots[i][1] not in y_group:
+            y_group.append(dots[i][1])
+    answer = abs((x_group[0]-x_group[1])*(y_group[0]-y_group[1]))
+    return answer
