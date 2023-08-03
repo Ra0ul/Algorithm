da80df5d15376ddbc51ad7bbe3b37b278487ed02
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-03 16:14:26 +0900

    [level 0] Title: 평행, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120875. 평행/평행.py
+++ 프로그래머스/lv0/120875. 평행/평행.py
@@ -1,23 +1,16 @@
+from itertools import combinations
+
 def solution(dots):
     answer = 0
-    """
-    기울기 = x값의 증가량/y값의 증가량
-    1-2랑 3-4랑 비교
-    1-3랑 2-4랑 비교
-    1-4랑 2-3랑 비교
-    """
-    rule = [(0,1,2,3),(0,2,1,3),(0,3,1,2)]
+    answer_list = []
     
-    for i in rule:
+    for i in combinations(dots,2):
         #i = (1,2,3,4)
-        left_x_size = dots[i[0]][0] - dots[i[1]][0]
-        left_y_size = dots[i[0]][1] - dots[i[1]][1]
-        right_x_size = dots[i[2]][0] - dots[i[3]][0]
-        right_y_size = dots[i[2]][1] - dots[i[3]][1]
+        answer_list.append((i[0][1] - i[1][1]) /(i[0][0] - i[1][0]))
         
-        if left_x_size/left_y_size == right_x_size/right_y_size:
-            return 1
-        else:
-            answer = 0
-        
+    print(answer_list)   # 012   -3-2-1
+    for i in range(len(answer_list)//2):
+    
+        if answer_list[i] == answer_list[-(i+1)]:
+            answer = 1
     return answer
