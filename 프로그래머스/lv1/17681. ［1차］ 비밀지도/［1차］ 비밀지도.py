923b373b3b2e8a8b560dbe9fc657efe704218c47
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-03 22:12:19 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.04 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/lv1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -5,19 +5,17 @@ def solution(n, arr1, arr2):
     secret_map=[]
     
     
-    
-    for i in arr1:
-        map1.append(int(bin(i)[2:]))
-    for i in arr2:
-        map2.append(int(bin(i)[2:]))
-    
-    
     for i in range(n):
+        map1.append(int(bin(arr1[i])[2:]))
+        map2.append(int(bin(arr2[i])[2:]))
         secret_map.append(str(map1[i]+map2[i]))
-        line =''
+        
         if len(secret_map[i]) < n:
             blank = '0'*(n-len(secret_map[i]))
             secret_map[i] = blank+secret_map[i]
+            
+        line =''
+        
         for u in secret_map[i]:
             if int(u) >= 1:
                 line += '#'
