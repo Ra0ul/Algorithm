943b3cec182489ae834431fb8c6e3f11eeda0daf
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-07 22:58:31 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,19 @@
+def solution(num):
+    count = 0
+    
+    while(num!=1):
+        if num == 1:
+            return 0
+        
+        elif num % 2 == 0:
+            num = num/2
+        else:
+            num = (num*3)+1
+        count += 1
+        
+        if count == 500:
+            break
+    
+    if count == 500 and num != 1:
+        return -1
+    return count
