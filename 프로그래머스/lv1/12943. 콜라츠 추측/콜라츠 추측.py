f748b962a90f158715dc9b8591f0ee4216e8acd1
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-08 09:20:02 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -1,11 +1,10 @@
 def solution(num):
     count = 0
+    if num == 1:
+        return 0
     
     while(num!=1):
-        if num == 1:
-            return 0
-        
-        elif num % 2 == 0:
+        if num % 2 == 0:
             num = num/2
         else:
             num = (num*3)+1
