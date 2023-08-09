a05bef4d416bdc453f2973303a5b4a31ad099e37
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-09 18:16:47 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 68.05 ms, Memory: 26.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,19 @@
+#효율성 테스트 시간 초과ㅠㅠ
+"""
+
+def solution(participant, completion):
+    answer = ''
+    for i in completion:
+        participant.remove(i)
+    return participant[0]
+    
+"""
+
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    for i_num, i in enumerate(completion):
+        if i != participant[i_num]:
+            print(i)
+            return participant[i_num]
+    return participant[len(participant)-1]
