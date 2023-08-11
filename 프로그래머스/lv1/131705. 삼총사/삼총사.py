e1e3238e98e51cf2018ddce077afbdab9fba0b3c
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-11 09:17:48 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.py
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.py
@@ -7,6 +7,7 @@ def solution(number):
     answer = 0
     # number 리스트에서 세명짝꿍으로 뽑아낼 수있는 경우의 수 구하기
     trio = list(combinations(number,3))
+    
     # 나온 수를 모두 더해서 0 이 된다면 삼총사!
     for i in trio:
         if sum(i) == 0:
