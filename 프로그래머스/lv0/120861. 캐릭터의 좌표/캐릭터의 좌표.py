bf974e4897fe4e65bba3765b07c6f22ec9b641b3
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-22 20:23:58 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.py
@@ -1,18 +1,12 @@
 def solution(keyinput, board):
-    limit = [board[0]//2,board[1]//2]
-    answer = []
-    x,y = 0, 0
-    
-    for i in keyinput:
-        if i == "up":
-            y = min(y+1, limit[1])
-        if i == "down":
-            y = max(y-1, -limit[1])
-        if i == "right":
-            x = min(x+1, limit[0])
-        if i == "left":
-            x = max(x-1, -limit[0])      
-        
-    answer = [x, y]
-    
-    return answer
+    x_lim,y_lim = board[0]//2,board[1]//2
+    move = {'left':(-1,0),'right':(1,0),'up':(0,1),'down':(0,-1)}
+    x,y = 0,0
+    for k in keyinput:
+        dx,dy = move[k]
+        if abs(x+dx)>x_lim or abs(y+dy)>y_lim:
+            continue
+        else:
+            x,y = x+dx,y+dy
+
+    return [x,y]
