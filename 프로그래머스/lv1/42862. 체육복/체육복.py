93d1cbed12884cad7307cb9fb49585ed665b4591
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-23 09:17:57 +0900

    [level 1] Title: 체육복, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,9 +1,6 @@
-def solution(n, lost, reserve):
-    answer = 0
-    check_list = {}
-    lost.sort()
+check_list = {}
 
-    # 도난 전 체크
+def uni_check(n, lost, reserve):
     for i in range(1, n+1):
         if i in reserve:
             check_list[i] = 2
@@ -12,9 +9,9 @@ def solution(n, lost, reserve):
         if i in lost:
             check_list[i] -=1
     print("전 : ", check_list, reserve)
-    
-    
-    #빌려주기
+
+
+def rent_uni(lost, reserve):
     for i in lost:
         if check_list[i] == 0:
             if i-1 in reserve and check_list[i-1]==2:
@@ -22,8 +19,19 @@ def solution(n, lost, reserve):
                 check_list[i] +=1
             elif i+1 in reserve and check_list[i+1]==2:
                 check_list[i+1] -=1
-                check_list[i] +=1
+                check_list[i] +=1    
         print("⭐️:",i, check_list)
+    
+    
+def solution(n, lost, reserve):
+    answer = 0
+    lost.sort()
+    
+    #체육복 체크
+    uni_check(n, lost, reserve)
+    
+    #빌려주기
+    rent_uni(lost, reserve)
     
     for i in check_list.values():
         if i >0:
