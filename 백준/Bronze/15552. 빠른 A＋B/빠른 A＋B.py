c449032e83a37310358071346132f6d8577039ea
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-08-31 09:41:32 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 1288 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,6 +1,6 @@
 import sys
 
-num = int(sys.stdin.readline().rstrip())
+num = int(sys.stdin.readline())
 for i in range(num):
-    a, b = map(int, sys.stdin.readline().rstrip().split())
+    a, b = map(int, sys.stdin.readline().split())
     print(a + b)
