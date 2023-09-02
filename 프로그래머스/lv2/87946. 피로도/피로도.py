7205c18bdf558e77caad464352d12cc75179eb6e
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-09-02 10:16:15 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/87946. 피로도/피로도.py
+++ 프로그래머스/lv2/87946. 피로도/피로도.py
@@ -5,17 +5,16 @@ def solution(k, dungeons):
     
     count_list = []
     ways = list(itertools.permutations(dungeons, len(dungeons)))
-    ways.sort()
     
-    for i in ways:
+    for i in ways: #모든 경우의 수 검사
         count = 0
         power = k
-        for u in i:
+        for u in i: #배열 순서대로 던전 통과
             if power >= u[0]:
                 power -= u[1]
                 count += 1
             else:
                 break
-        count_list.append(count)   
+        count_list.append(count)  #몇개 지났는지 저장
             
-    return max(count_list)
+    return max(count_list) #가장 많이 지난 것은?
