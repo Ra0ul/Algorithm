48d9e8f420d6d897efcf3d992b6d3cd02b9e5c95
Ra0ul <79742001+Ra0ul@users.noreply.github.com>
2023-10-24 10:43:07 +0900

    [level unrated] Title: 간단한 논리 연산, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
+++ 프로그래머스/unrated/181917. 간단한 논리 연산/간단한 논리 연산.py
@@ -1,4 +1,5 @@
 def solution(x1, x2, x3, x4):
     answer = True
-    answer = (int(x1)+int(x2))*(int(x3)+int(x4))
+    x1, x2, x3, x4 = map(int, [x1, x2, x3, x4])
+    answer = (x1+x2)*(x3+x4)
     return bool(answer)
